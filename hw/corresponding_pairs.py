b32897c981b07d42d8624c4dc63239ab83962eea
gsoulealded <gsoule.alded@gmail.com>
2026-02-02 19:21:37 +0300

    Add files via upload

---
+++ hw/corresponding_pairs.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+__all__ = (
+    'corresponding_pairs',
+)
+
+
+def corresponding_pairs(arr1: list[Any], arr2: list[Any]) -> list[tuple[Any, Any]]:
+    """
+    Функция должна возвращать соответствующие элементы двух массивов:
+    corresponding_pairs([1, 2], [3, 4]) == [(1, 3), (2, 4)]
+    """
+    returned_array = []
+    minimal_length = min(len(arr1), len(arr2))
+    for i in range(minimal_length):
+        returned_array.append((arr1[i], arr2[i]))
+    return returned_array
